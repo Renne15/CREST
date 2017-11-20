f39e265b46babaf5cf797c426d81947714cc6469
Renne <renne00.xv@gmail.com>
2017-11-20 16:31:09 +0900

    update

--- conn.py
+++ conn.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 
-filePath_json = "../conll2015_discourse/data/output.json"
+filePath_json = "./data/output.json"
 
 with open(filePath_json, 'r') as readjson:
     data_json = {i : json.loads(line) for i, line in enumerate(readjson)}
@@ -25,19 +25,19 @@ for line, value in data_json.items():
         sense[value["Sense"][0]]["count"] = 0
         sense[value["Sense"][0]]["conn"] = []
         if "conn_name" in value:
-            sense[value["Sense"][0]]["conn"] = value["conn_name"]
+            sense[value["Sense"][0]]["conn"].append(value["conn_name"])
     elif value["Sense"][0] in sense:
         sense[value["Sense"][0]]["count"] += 1
         if "conn_name" in value:
             if value["conn_name"] not in sense[value["Sense"][0]]["conn"]:
-                sense[value["Sense"][0]]["conn"] += ', '+value["conn_name"]
+                sense[value["Sense"][0]]["conn"].append(value["conn_name"])
 
-conn_df = pd.DataFrame.from_dict(conn)
-sense_df = pd.DataFrame.from_dict(sense)
+conn_df = pd.DataFrame(conn, index=["count","sense"])
+sense_df = pd.DataFrame(sense, index=["count","conn"])
 conn_df = conn_df.T
 sense_df = sense_df.T
-conn_df.sort_values(by="count", ascending=False)
-sense_df.sort_values(by="count", ascending=False)
+conn_df = conn_df.sort_values(by=["count"], ascending=False)
+sense_df = sense_df.sort_values(by=["count"], ascending=False)
 
 print(conn_df)
 print(sense_df)
